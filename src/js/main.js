dcc5376f00ee8eb2445461a51f9e89a2320bb5e2
Adelinasul <sultanbekovvaa2003@gmail.com>
2023-04-04 14:19:35 +0600

    add: link

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,20 @@
+let tabs = document.querySelectorAll(".tabs__tab-nav-btn")
+let tabContent = document.querySelectorAll(".tabs__content-item");
 
+tabs.forEach(el => {    el.addEventListener("click",() => {
+    let item = el.getAttribute("data-tab") ;
+    let current = document.getElementById(item)
+    tabs.forEach(el => {
+        el.classList.remove("active")
+    })
+    tabContent.forEach(el => {
+        el.classList.remove('active')        })
+    el.classList.add("active")
+    console.log(current)
+    current.classList.add("active")
+})
+
+})
 
 let btn = document.querySelector('.header__btn')
 let overlay = document.querySelector('.overlay')
@@ -24,4 +40,6 @@ burger.addEventListener('click', () => {
     list.classList.toggle('show')
     burger.classList.toggle('show')
 })
+new WOW().init();
 AOS.init();
+
